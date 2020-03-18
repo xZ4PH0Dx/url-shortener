12b8ad6bd326b1403070a4337b0f8a162a8d8136
root <root@DESKTOP-8QJAM3L.localdomain>
2020-03-18 05:53:50 +0300

    GetById testcases

--- internal/publicapi/router_test.go
+++ internal/publicapi/router_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"url_shortener"
 	"url_shortener/internal/mocks"
@@ -36,12 +37,39 @@ var (
 func TestService_GetById(t *testing.T) {
 	var testUrl url_shortener.Url
 
+	type args struct {
+		x interface{}
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "success",
+			args: args{
+				x: 1,
+			},
+			want: u,
+		},
+		{
+			name: "success nil",
+			args: args{
+				x: 2,
+			},
+			want: url_shortener.Url{},
+		},
+	}
+
 	db := &mocks.URLRepository{
 		ByIdFn: func(ctx context.Context, id int) (url url_shortener.Url, err error) {
-			url = url_shortener.Url{
-				ID:   1,
-				Url:  "http://google.com",
-				Code: "so1gFSl5",
+			if id == 1 {
+				url = url_shortener.Url{
+					ID:   1,
+					Url:  "http://google.com",
+					Code: "so1gFSl5",
+				}
 			}
 			return
 		},
@@ -51,15 +79,30 @@ func TestService_GetById(t *testing.T) {
 	srv := httptest.NewServer(r)
 	defer srv.Close()
 
-	resp, err := http.Get(srv.URL + "/urls/1")
-	if err != nil {
-		t.Fatal(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var strId string
+			switch v := tt.args.x.(type) {
+			case int:
+				strId = strconv.Itoa(v)
+			case string:
+				strId = v
+			}
+			uri := "/urls/" + strId
+			resp, err := http.Get(srv.URL + uri)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			err = json.NewDecoder(resp.Body).Decode(&testUrl)
+
+			if testUrl != tt.want {
+				t.Errorf("GetById() = %v, want %v", testUrl, tt.want)
+			}
+		})
 	}
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&testUrl)
-
-	assert.Equal(t, u, testUrl)
+	//assert.Equal(t, u, testUrl)
 }
 
 func TestService_CreateUrl(t *testing.T) {
